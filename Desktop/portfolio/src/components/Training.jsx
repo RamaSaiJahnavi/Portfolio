4c5cc3ab1e50d23616c19514a93439b4886331d3
P RAMA SAI JAHNAVI <pottapogulajahnavi@gmail.com>
2026-03-08 21:36:20 +0530

    Update Training certificate link to Google Drive

--- Desktop/portfolio/src/components/Training.jsx
+++ Desktop/portfolio/src/components/Training.jsx
@@ -26,7 +26,7 @@ function Training() {
               <li key={i}>{detail}</li>
             ))}
           </ul>
-          <a href="#" className="training-link" target="_blank" rel="noopener noreferrer">View Certificate</a>
+          <a href="https://drive.google.com/file/d/1N8lHIPzpeKUJwBH_rmJPUuI6uHyxbCUs/view" className="training-link" target="_blank" rel="noopener noreferrer">View Certificate</a>
         </div>
       ))}
     </div>
